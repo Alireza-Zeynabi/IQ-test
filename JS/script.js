30eae8c4ec454f0dc2a9eba54f883146c1fe220f
Alireza-Zeynabi <zeynabialireza@gmail.com>
2023-02-26 15:48:02 +0330

    IQ test new v

---
+++ JS/script.js
@@ -0,0 +1,300 @@
+const body = document.body
+
+// create start btn
+const startBtn = document.createElement('div')
+startBtn.classList.add('startBtn')
+startBtn.innerText = 'start Test'
+body.appendChild(startBtn)
+// =-=-=-=-=-=-=-==-=-==-=-
+
+// create test page 
+
+// -=-=-=-=-==-=-=-=-=-=-=-
+
+// create question box in testCon
+
+// =-=-=-=-=-=-=-=-=-=-=-=-
+
+// create Answer box in testCon
+
+const testSection = document.createElement('section')
+testSection.classList.add('testSection')
+body.appendChild(testSection)
+
+
+for (let i = 30; i >= 1; i--) {
+
+    const testContainer = document.createElement('div')
+    testContainer.classList.add('testContainer')
+    testContainer.classList.add('container' + [i])
+
+    testSection.appendChild(testContainer)
+
+    let pagesCounter = document.createElement('div')
+    pagesCounter.classList.add('pagesCounter')
+    testContainer.appendChild(pagesCounter)
+    let pageCounterSpan = document.createElement('span')
+    pagesCounter.appendChild(pageCounterSpan)
+    pageCounterSpan.append(i + '/30')
+
+
+    // create questionBox
+    let questionBox = document.createElement('div')
+    questionBox.classList.add('questionBox')
+    questionBox.classList.add('test' + [i])
+    testContainer.appendChild(questionBox)
+
+    //  adding image to questionBox
+    let questionBoxImg = document.createElement('img')
+    questionBoxImg.setAttribute('src', 'Images/' + [i] + '/test' + [i] + '.png');
+    // src="Images/[i]/test[i].png" 
+    questionBox.appendChild(questionBoxImg)
+    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+    // create questionBox
+
+    let AnswerBox = document.createElement('div');
+    AnswerBox.classList.add('answerBox')
+    AnswerBox.classList.add('answerBox' + [i])
+    testContainer.appendChild(AnswerBox)
+
+    if (i <= 12) {
+
+        for (let j = 1; j <= 6; j++) {
+            let options = document.createElement('img')
+            options.setAttribute('src', 'Images/' + [i] + '/' + [i] + '-' + [j] + '.png')
+            // options.setAttribute('id', 'trueAnswer')
+            options.setAttribute('id', [j])
+            options.classList.add('options')
+            // src="Images/1/1-1.png"
+            AnswerBox.appendChild(options)
+
+
+            options.addEventListener('click', next)
+
+            function next() {
+                // options.style ='background-color:red'
+                AnswerBox.parentElement.style = "display:none"
+            }
+        }
+    } else {
+        for (let x = 1; x <= 8; x++) {
+            let options = document.createElement('img')
+            options.setAttribute('src', 'Images/' + [i] + '/' + [i] + '-' + [x] + '.png')
+            //   options.setAttribute('id', 'trueAnswer')
+            options.setAttribute('id', [x])
+            options.classList.add('options')
+            // src="Images/1/1-1.png"
+            AnswerBox.appendChild(options)
+
+            options.addEventListener('click', next)
+
+            function next() {
+                // options.style ='background-color:red'
+                AnswerBox.parentElement.style = "display:none"
+            }
+
+        }
+    }
+
+
+}
+
+
+// find true answers
+//  questions
+
+document.querySelector('.answerBox1 img:nth-child(3) ')
+    .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox2 img:nth-child(1) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox3 img:nth-child(5) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox4 img:nth-child(5) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox5 img:nth-child(2) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox6 img:nth-child(1) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox7 img:nth-child(2) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox8 img:nth-child(2) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox9 img:nth-child(2) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox10 img:nth-child(6) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox11 img:nth-child(4) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox12 img:nth-child(1) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox13 img:nth-child(4) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox14 img:nth-child(7) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox15 img:nth-child(2) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox16 img:nth-child(3) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox17 img:nth-child(1) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox18 img:nth-child(6) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox19 img:nth-child(5) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox20 img:nth-child(8) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox21 img:nth-child(4) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox22 img:nth-child(4) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox23 img:nth-child(7) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox24 img:nth-child(6) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox25 img:nth-child(4) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox26 img:nth-child(7) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox27 img:nth-child(7) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox28 img:nth-child(3) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox29 img:nth-child(2) ')
+        .classList.add('trueAnswer'),
+
+    document.querySelector('.answerBox30 img:nth-child(8) ')
+        .classList.add('trueAnswer');
+
+
+// add score after click on true options
+
+// select all option img 
+let alTrueAnswers = document.querySelectorAll('.options')
+
+alTrueAnswers.forEach(element => { element.addEventListener('click', () => { givingScore(element) }) });
+
+
+// Counting scores
+let score = 0
+function givingScore(element) {
+    if (element.classList.contains('trueAnswer')) {
+
+        score += 1
+        console.log(score)
+    }
+
+
+}
+
+// create Results page
+
+let ResultsPage = document.createElement('div')
+ResultsPage.classList.add('ResultsPage')
+body.append(ResultsPage)
+
+let finalScore = document.createElement('div')
+finalScore.classList.add('finalScore')
+ResultsPage.appendChild(finalScore)
+
+
+let finalSentence = document.createElement('div')
+finalSentence.classList.add('finalSentence')
+ResultsPage.appendChild(finalSentence)
+
+
+
+// show  Results page
+
+let lastTestPage = document.querySelector('.answerBox30')
+
+lastTestPage.addEventListener('click', av)
+
+function av() {
+
+    document.querySelector('.ResultsPage').style.display = 'block'
+
+    finalScore.innerText = 'your score is' + ' ' + ' ' + ' ' + score
+
+    if (score < 5) {
+        finalSentence.innerText = 'trust me, go kill yourself😃'
+    }
+    if (score >= 5) {
+        finalSentence.innerText = 'trust me, go kill yourself😃'
+    }
+    if (score >= 10) {
+
+        finalSentence.innerText = 'you fuckdup but its not so bad (pakhme 😆)'
+    }
+    if (score >= 15) {
+
+        finalSentence.innerText = 'sorry i think you are blind '
+    }
+    if (score >= 20) {
+
+        finalSentence.innerText = 'Look at the fucking monitor and get a better score (Its not too bad, your head is not empty ) '
+    }
+    if (score >= 25) {
+        finalSentence.innerText = 'okey okey good (Do not be snobbish You are still an idiot with more practice)'
+
+    }
+    if (score == 30) {
+        finalSentence.innerText = 'Congratulations, you are out of stupid mode'
+
+    }
+
+}
+console.log(score)
+
+
+
+
+
+
+
+
+
+
+
+// =-=-=-=-=-=-=-=-=-=-=-=-
+// addEventListener to startBtn
+// open test page
+startBtn.addEventListener('click', openTestPage)
+function openTestPage() {
+
+    document.querySelector('.startBtn').style.display = 'none'
+
+    document.querySelector('.testSection').style = 'display:flex; justify-content: center;'
+
+}
+
+
+
---
+++ iq1/IQ-TEST/JS/script.js
@@ -0,0 +1,213 @@
+// Selecting body
+let body = document.querySelector("body");
+
+// Create <span> for timer on top of viewport
+let span = document.createElement("span");
+// Set attribute for span
+span.setAttribute("id", "timer");
+// Appending <span> to body
+body.appendChild(span);
+
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+
+// ---> Creating login form
+// Create <div> tag
+let loginBox = document.createElement("div");
+// Set class for loginBox
+loginBox.classList.add("loginBox");
+// Appending loginBox to body
+body.appendChild(loginBox);
+
+// Create <h2> for title
+let h2 = document.createElement("h2");
+// Set text for title
+h2.innerText = "IQ TEST";
+// Appendig <h2> tag to loginBox
+loginBox.appendChild(h2);
+
+// Craeting <form> tag
+let form = document.createElement("form");
+// Set attribute for form
+form.setAttribute("action", "#");
+// Appending form to loginBox
+loginBox.appendChild(form);
+
+// Create <div> tag for user information
+let userBox = document.createElement("div");
+// Set class for userBox
+userBox.classList.add("userBox");
+// Appending userBox to form
+form.appendChild(userBox);
+
+// Creating <input> tag
+let input = document.createElement("input");
+// Set class for input
+input.classList.add('userName');
+// Set attribute for input
+input.setAttribute("type", "text");
+// Appending input to userBox
+userBox.appendChild(input);
+
+// Creating label for input
+let label = document.createElement("label");
+// Set text for label
+label.innerText = "Name";
+// Appending label to userBox
+userBox.appendChild(label);
+
+// Craeting <a> tag for submit button animation
+let a = document.createElement("a");
+// Craeting 4 spans for animation with loop
+for (let x = 0; x < 4; x++) {
+  let span = document.createElement("span");
+  // Appending span to a
+  a.appendChild(span);
+}
+
+// Creating submit button
+let submit = document.createElement("input");
+// Set attribute for submit button
+submit.setAttribute("type", "submit");
+// Appending submit button to a
+a.appendChild(submit);
+
+// Appending a to form
+form.appendChild(a);
+
+// Creating <p> tag
+let p = document.createElement("p");
+// Set text for p
+p.innerText = "* IQ test will start after submitting name *";
+// Appending p to loginBox
+loginBox.appendChild(p);
+
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+
+// Geting login form input value
+let userName = document.querySelectorAll('.userName').value;
+
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+
+// Create <section> tag for test
+let testSection = document.createElement("section");
+// Adding testSection to body
+body.appendChild(testSection);
+// Create loop to create 30 container page
+for (let i = 1; i <= 30; i++) {
+  // Create <div> tag for container
+  let testContainer = document.createElement("div");
+  // Set class for All testContainer
+  testContainer.classList.add("testContainer");
+  // Set class for testContainer one by one
+  testContainer.classList.add("container" + [i]);
+
+  // Appending testContainer to testSection
+  testSection.appendChild(testContainer);
+
+  // Create <div> tag for counter pages
+  let pageCounter = document.createElement("div");
+  // Set class for pageCounter
+  pageCounter.classList.add("pageCounter");
+
+  // Appending pageCounter to testContainer
+  testContainer.appendChild(pageCounter);
+
+  // Create span for pageCounter
+  let pageCounterSpan = document.createElement("span");
+  // Appending pageCounterSpan to pageCounter
+  pageCounter.appendChild(pageCounterSpan);
+  // Append span counter
+  pageCounterSpan.append(i + "/30");
+
+  // Create <div> tag for question test
+  let questionTest = document.createElement("div");
+  // Set class for All questionTest
+  questionTest.classList.add("questionTest");
+  // Set class for questionTest one by one
+  questionTest.classList.add("test" + i);
+  // Appending questionTest to testContainer
+  testContainer.appendChild(questionTest);
+
+  // Create <img> for test question
+  let testImg = document.createElement("img");
+  // Set attributes for one by one of <img> tag
+  testImg.setAttribute("src", "Images/" + [i] + "/test" + [i] + ".png");
+  // Appending testImg to questionTest
+  questionTest.appendChild(testImg);
+
+  // Create <div> tag for answer box
+  let answerBox = document.createElement("div");
+  // Set class for answer box
+  answerBox.classList.add("answerBox");
+  // Appending answerBox to testContainer
+  testContainer.appendChild(answerBox);
+
+  // Create conditional command for test answer
+  if (i <= 12) {
+    // loop for question one to twelve
+    for (let j = 1; j <= 6; j++) {
+      // Creating <img> tag for test answer
+      let option = document.createElement("img");
+      option.classList.add("option");
+      option.setAttribute(
+        "src",
+        "Images/" + [i] + "/" + [i] + "-" + [j] + ".png"
+      );
+      answerBox.appendChild(option);
+    }
+  } else {
+    for (let j = 1; j <= 8; j++) {
+      let option = document.createElement("img");
+      option.classList.add("option");
+      option.setAttribute(
+        "src",
+        "Images/" + [i] + "/" + [i] + "-" + [j] + ".png"
+      );
+      answerBox.appendChild(option);
+    }
+  }
+}
+
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+
+// ---> Events
+a.addEventListener("click", questionPage);
+
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+
+// selecting answer image
+let testAnswer = document.querySelectorAll(".answerBox img");
+// loop for next container
+for (let x = 0; x < testAnswer.length; x++) {
+  // addEventListener to when click at answer goes to next page
+  testAnswer[x].addEventListener("click", nextContainer);
+  // Function to 'display: none' previous page and 'display: flex' next page
+  function nextContainer() {
+    // Varibale for selecting test answer parent element
+    let container = testAnswer[x].parentElement;
+    // 'display: none' previous page
+    container.parentElement.style.display = "none";
+    // Varibale for selecting next element of container
+    let nextContainer = container.parentElement.nextElementSibling;
+    // 'display: flex' next page
+    nextContainer.style.display = "flex";
+  }
+}
+
+// Function for when form is submitted change display!
+function questionPage(e) {
+  e.preventDefault();
+  document.querySelector(".loginBox").style.display = "none";
+  document.querySelector(".container1").style.display = "flex";
+
+  // Create timer for page
+  // Selecting time according to secend
+  let sec = 500;
+  let time = setInterval(myTimer, 1000);
+
+  function myTimer() {
+    let timer = document.getElementById("timer");
+    timer.innerHTML = "Time left : " + sec + "sec";
+    sec--;
+  }
+}
